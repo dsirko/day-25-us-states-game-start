ed5cc895d3a6d832a97968a639c0fa6e37bdbbc5
Dmytro <dimasirko@gmail.com>
2024-12-27 10:45:06 +0200

    added show states on the map. Added counting scores

---
+++ SignState.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+FONT = ("Arial", 10, "normal")
+ALIGNMENT = "center"
+
+
+class SignState(Turtle):
+    def __init__(self, name, x, y):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("red")
+        self.goto(x, y)
+        self.name = name
+
+    def write_state(self):
+        self.write(f"{self.name}", align=ALIGNMENT, font=FONT)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
-# import csv
 import pandas as pd
+from SignState import SignState
+
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -11,39 +12,32 @@ turtle.shape(image)
 df = pd.read_csv("50_states.csv")
 dict_from_csv = df.to_dict(orient="records")
 
-
-
-# with open("50_states.csv", "r") as states:
-#     reader = csv.DictReader(states)
-#     dict_from_csv = [row for row in reader]
-#
-# print(dict_from_csv)
-# print(dict_from_csv[49]["state"])
-
 game_is_on = True
+guessed_states = []
 
 while game_is_on:
 
-    answer_state = turtle.textinput(title="Guess the State", prompt="What's another state's name?").title()
+    answer_state = turtle.textinput(title=f"{len(guessed_states)}/{len(dict_from_csv)} States Correct", prompt="What's another state's name?").title()
 
     for st in dict_from_csv:
         if answer_state == st["state"]:
             print(f"it is finded {answer_state}\n"
                   f"cordinates: x:{st["x"]}, y:{st["y"]}")
+            state_to_show = SignState(answer_state, st["x"], st["y"])
+            state_to_show.write_state()
+            guessed_states.append(answer_state)
+            print(len(guessed_states))
+
         # elif answer_state != st["state"]:
         #     print(f"{answer_state} didn't find!")
         # print(st["state"], st["x"], st["y"])
 
-
 #     print(answer_state)
-
 
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 #
-#
 # turtle.onscreenclick(get_mouse_click_coor)
-
 
 
 screen.mainloop()
